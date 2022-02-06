b7ca7d70ebc0aa632a8dab4e10ea6f0371ed0663
AdalZanabria <adal.zanabria@gmail.com>
2022-02-06 13:30:08 -0600

    Cambio de dependecia de faker.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {faker} = require('@faker-js/faker');
+const { faker } = require('@faker-js/faker');
 
 const app = express();
 const port = 3000;
@@ -22,12 +22,13 @@ app.get('/tasks', (req, res) => {
   ]);
 });
 
+// Todos los endpoints específicos deben de ir antes que los dinámicos
 //Los dos puntos antes de id significa que va a ser un parametro
 app.get('/tasks/:id', (req, res) => {
   const { id } = req.params; //Este nombre {id} tiene que ser el mismo que el nombre del endpoint :id
   res.json({
     id,
-    caso: 'No hay internet.',
+    caso: 'Se acabó el tonner.',
   });
 });
 
@@ -60,15 +61,18 @@ app.get('/people', (req, res) => {
     },
   ]);
 });
+/* Así como hay endpoint /tasks/{id} y endpoint /people/{id} para tener tareas y personas en
+específico, también puede haber /people/{id}/tasks para obtener las tareas de ese usuario.
+*/
 
 /*Según el size que mandemos por medio del query en el endpoint será la
 cantidad de objetos mostrados con 10 por defecto, ej: /products?size=20
 */
 app.get('/products', (req, res) => {
   const products = [];
-  const {size} = req.query;
+  const { size } = req.query;
   const limit = size || 10;
-  for (let index = 0; index < limit; index++){
+  for (let index = 0; index < limit; index++) {
     products.push({
       name: faker.commerce.productName(),
       price: parseInt(faker.commerce.price(), 10),
@@ -79,67 +83,56 @@ app.get('/products', (req, res) => {
 });
 
 app.get('/products/:id', (req, res) => {
-  const {id} = req.params;
-  res.json(
-    {
-      id,
-      name: 'Producto 1',
-      price: 1000
-    }
-  )
-})
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Producto 1',
+    price: 1000,
+  });
+});
 
 app.get('/categories', (req, res) => {
-  res.json(
-    [
+  res.json([
     {
-      name: 'Ropa'
+      name: 'Ropa',
     },
     {
-      name: 'Calzado'
+      name: 'Calzado',
     },
     {
-      name: 'Accesorios'
-    }
-  ]
-  )
-})
+      name: 'Accesorios',
+    },
+  ]);
+});
 
 app.get('/categories/:id', (req, res) => {
-  const {id} = req.params;
-  res.json(
-    {
-      id,
-      name: 'Ropa'
-    }
-  )
-})
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Ropa',
+  });
+});
 
-app. get('/categories/:categoryId/products/:productId', (req, res) => {
-  const {categoryId, productId} = req.params;
-  res.json(
-    {
-      categoryId,
-      productId,
-    }
-  )
-})
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
+});
 
 app.get('/users', (req, res) => {
-  const {limit, offset} =  req.query;
-  if(limit && offset){
+  const { limit, offset } = req.query;
+  if (limit && offset) {
     res.json({
       limit,
-      offset
-    })
-  }else{
-    res.send('No hay parametros')
+      offset,
+    });
+  } else {
+    res.send('No hay parametros');
   }
-})
+});
 
-/* Así como hay endpoint /tasks/{id} y endpoint /people/{id} para tener tareas y personas en
-específico, también puede haber /people/{id}/tasks para obtener las tareas de ese usuario.
-*/
 app.listen(port, () => {
   console.log(`Ejecutando desde: http://localhost:${port}`);
 });
