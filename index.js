57847cdfc3c77b4a7f363ee9f5f5c584eb100121
AdalZanabria <adal.zanabria@gmail.com>
2022-02-14 16:00:32 -0600

    Agregué middlewares de error y boom para productos.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routerApi = require('./routes');
+const { logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,10 @@ app.get('/', (req, res) => {
 });
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Ejecutando desde: http://localhost:${port}`);
--- routes/products.routing.js
+++ routes/products.routing.js
@@ -12,10 +12,14 @@ router.get('/', async (req, res) => {
   res.json(products);
 });
 
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const product = await service.findOne(id);
-  res.json(product);
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await service.findOne(id);
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/', async (req, res) => {
@@ -24,16 +28,14 @@ router.post('/', async (req, res) => {
   res.status(201).json(newProduct);
 });
 
-router.patch('/:id', async (req, res) => {
+router.patch('/:id', async (req, res, next) => {
   try {
     const { id } = req.params;
     const body = req.body;
     const product = await service.update(id, body);
     res.json(product);
   } catch (error) {
-    res.status(404).json({
-      message: error.message,
-    });
+    next(error);
   }
 });
 
