c3c1fe2469bc5bc8e50a16d1c6bbdafeda2a1802
AdalZanabria <adal.zanabria@gmail.com>
2022-02-15 02:29:19 -0600

    Agregué excepciones CORS.

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
-const { logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler');
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+//CORS
+const whitelist = ['http://localhost:8080', '127.0.0.1:8080'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido.'));
+    }
+  },
+};
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola, este es mi server de prueba con express.');
