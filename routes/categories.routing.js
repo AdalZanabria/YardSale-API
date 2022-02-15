3e03a40ab18af5ca4946b71145280b9d5777ddb8
AdalZanabria <adal.zanabria@gmail.com>
2022-02-15 01:28:21 -0600

    Agregué validaciones a Categories.

--- routes/categories.routing.js
+++ routes/categories.routing.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const CategoriesService = require('../services/categories.service');
+const validatorHandler = require('../middlewares/validator.handler');
+const {
+  createCategorySchema,
+  updateCategorySchema,
+  getCategorySchema,
+} = require('../schemas/categories.schema');
 
 const router = express.Router();
 const service = new CategoriesService();
@@ -9,15 +15,19 @@ router.get('/', async (req, res) => {
   res.json(categories);
 });
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const category = await service.findOne(id);
-    res.json(category);
-  } catch (error) {
-    next(error);
+router.get(
+  '/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const category = await service.findOne(id);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 // Un producto dentro de una categoría
 router.get('/:categoryId/products/:productId', (req, res) => {
@@ -28,22 +38,31 @@ router.get('/:categoryId/products/:productId', (req, res) => {
   });
 });
 
-router.post('/', async (req, res) => {
-  const body = req.body;
-  const newCategory = await service.create(body);
-  res.status(201).json(newCategory);
-});
-
-router.patch('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
+router.post(
+  '/',
+  validatorHandler(createCategorySchema, 'body'),
+  async (req, res) => {
     const body = req.body;
-    const category = await service.update(id, body);
-    res.json(category);
-  } catch (error) {
-    next(error);
+    const newCategory = await service.create(body);
+    res.status(201).json(newCategory);
   }
-});
+);
+
+router.patch(
+  '/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  validatorHandler(updateCategorySchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const category = await service.update(id, body);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 router.delete('/:id', async (req, res) => {
   try {
---
+++ schemas/categories.schema.js
@@ -0,0 +1,22 @@
+const Joi = require('joi');
+
+const name = Joi.string().min(3).max(15);
+const id = Joi.number().integer();
+
+const createCategorySchema = Joi.object({
+  name: name.required(),
+});
+
+const updateCategorySchema = Joi.object({
+  name: name,
+});
+
+const getCategorySchema = Joi.object({
+  id: id.required(),
+});
+
+module.exports = {
+  createCategorySchema,
+  updateCategorySchema,
+  getCategorySchema,
+};
