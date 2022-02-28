804a2d0819c4d7806c9fb5d50d532c13ceffca54
AdalZanabria <adal.zanabria@gmail.com>
2022-02-27 21:38:29 -0600

    Agregado sequelize-cli y archivos para configurar migración.

--- db/config.js
+++ db/config.js
@@ -1,20 +1,17 @@
-const { Sequelize } = require('sequelize');
-
 const { config } = require('../config/config');
-const setupModels = require('../db/models');
 
 const dbType = 'postgres'; //postgres o mysql
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `${dbType}://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-const sequelize = new Sequelize(URI, {
-  dialect: dbType,
-  logging: true,
-});
-
-setupModels(sequelize);
-
-sequelize.sync();
-
-module.exports = sequelize;
+module.exports = {
+  development: {
+    url: URI,
+    dialect: dbType,
+  },
+  production: {
+    url: URI,
+    dialect: dbType,
+  },
+};
