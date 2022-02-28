a98a4742abd706e97d53f0aa144685fe8d0ef380
AdalZanabria <adal.zanabria@gmail.com>
2022-02-28 13:39:11 -0600

    Creados modelos para categorias y productos con relación 1-N y su migración.

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -2,17 +2,20 @@ const Joi = require('joi');
 
 const id = Joi.string().uuid();
 const name = Joi.string().min(3).max(15);
+const description = Joi.string().min(5);
 const price = Joi.number().integer().min(10);
 const image = Joi.string().uri();
 
 const createProductSchema = Joi.object({
   name: name.required(),
+  description: description.required(),
   price: price.required(),
   image: image.required(),
 });
 
 const updateProductSchema = Joi.object({
   name: name,
+  description: description,
   price: price,
   image: image,
 });
