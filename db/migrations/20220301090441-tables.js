e32769441ad098e4af2bce3b1f8f706fc8a73e4e
AdalZanabria <adal.zanabria@gmail.com>
2022-03-01 10:59:34 -0600

    Modificación de migración de tabla ORDER para no usar datos virtuales.

--- db/migrations/20220301090441-tables.js
+++ db/migrations/20220301090441-tables.js
@@ -1,10 +1,11 @@
 'use strict';
+const { DataTypes, Sequelize } = require('sequelize');
 
 const { UserSchema, USER_TABLE } = require('../models/user.model');
 const { CustomerSchema, CUSTOMER_TABLE } = require('../models/customer.model');
 const { CategorySchema, CATEGORY_TABLE } = require('../models/category.model');
 const { ProductSchema, PRODUCT_TABLE } = require('../models/product.model');
-const { OrderSchema, ORDER_TABLE } = require('../models/order.model');
+const { ORDER_TABLE } = require('../models/order.model');
 const { OrderProductSchema, ORDER_PRODUCT_TABLE } = require('../models/order-product.model');
 
 module.exports = {
@@ -13,7 +14,31 @@ module.exports = {
     await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
     await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
     await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      }
+    });
     await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrderProductSchema);
   },
 
