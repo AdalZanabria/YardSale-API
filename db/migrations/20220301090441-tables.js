c7c3748003cc44cfd0c525dd0104db3c66ee094e
AdalZanabria <adal.zanabria@gmail.com>
2022-03-01 11:03:30 -0600

    fix de migración Order_table

--- db/migrations/20220301090441-tables.js
+++ db/migrations/20220301090441-tables.js
@@ -6,10 +6,13 @@ const { CustomerSchema, CUSTOMER_TABLE } = require('../models/customer.model');
 const { CategorySchema, CATEGORY_TABLE } = require('../models/category.model');
 const { ProductSchema, PRODUCT_TABLE } = require('../models/product.model');
 const { ORDER_TABLE } = require('../models/order.model');
-const { OrderProductSchema, ORDER_PRODUCT_TABLE } = require('../models/order-product.model');
+const {
+  OrderProductSchema,
+  ORDER_PRODUCT_TABLE,
+} = require('../models/order-product.model');
 
 module.exports = {
-  async up (queryInterface) {
+  async up(queryInterface) {
     await queryInterface.createTable(USER_TABLE, UserSchema);
     await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
     await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
@@ -37,17 +40,17 @@ module.exports = {
         type: DataTypes.DATE,
         field: 'created_at',
         defaultValue: Sequelize.NOW,
-      }
+      },
     });
     await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrderProductSchema);
   },
 
-  async down (queryInterface) {
+  async down(queryInterface) {
     await queryInterface.dropTable(USER_TABLE);
     await queryInterface.dropTable(CUSTOMER_TABLE);
     await queryInterface.dropTable(CATEGORY_TABLE);
     await queryInterface.dropTable(PRODUCT_TABLE);
     await queryInterface.dropTable(ORDER_TABLE);
     await queryInterface.dropTable(ORDER_PRODUCT_TABLE);
-  }
+  },
 };
