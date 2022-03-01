7a03b18d5b7b6c1c7200a7a369cb7c9613afd2f0
AdalZanabria <adal.zanabria@gmail.com>
2022-03-01 10:38:49 -0600

    Unificadas las migraciones un un único archivo.

--- db/migrations/20220301090441-tables.js
+++ db/migrations/20220301090441-tables.js
@@ -1,21 +1,28 @@
 'use strict';
 
+const { UserSchema, USER_TABLE } = require('../models/user.model');
+const { CustomerSchema, CUSTOMER_TABLE } = require('../models/customer.model');
+const { CategorySchema, CATEGORY_TABLE } = require('../models/category.model');
+const { ProductSchema, PRODUCT_TABLE } = require('../models/product.model');
+const { OrderSchema, ORDER_TABLE } = require('../models/order.model');
+const { OrderProductSchema, ORDER_PRODUCT_TABLE } = require('../models/order-product.model');
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up (queryInterface) {
+    await queryInterface.createTable(USER_TABLE, UserSchema);
+    await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
+    await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
+    await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
+    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrderProductSchema);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down (queryInterface) {
+    await queryInterface.dropTable(USER_TABLE);
+    await queryInterface.dropTable(CUSTOMER_TABLE);
+    await queryInterface.dropTable(CATEGORY_TABLE);
+    await queryInterface.dropTable(PRODUCT_TABLE);
+    await queryInterface.dropTable(ORDER_TABLE);
+    await queryInterface.dropTable(ORDER_PRODUCT_TABLE);
   }
 };
